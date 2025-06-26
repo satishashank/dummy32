c67851b731064ce8e966bb3e278313b1b465cb20
shashanksati123 <shashanksati123@gmail.com>
2025-06-26 15:57:54 +0530

    Pipelined: basic regfile of 32x32 size

---
+++ registerFile.sv
@@ -0,0 +1,21 @@
+`timescale 1ns/1ps
+module registerFile(
+    output logic [31:0] reg1,
+    output logic [31:0] reg2,
+    input logic [31:0] writeData,
+    input logic [4:0] addr1,
+    input logic [4:0] addr2,
+    input logic [4:0] writeAddr,
+    input logic writeEn,
+    input logic clk
+  );
+
+  logic [31:0] registers [31:0];
+  assign reg1 = |addr1 ? registers[addr1] : 0;
+  assign reg2 = |addr2 ? registers[addr2] : 0;
+  always@(posedge clk)
+  begin
+    if (writeEn & |writeAddr)
+      registers[writeAddr] <= writeData;
+  end
+endmodule
