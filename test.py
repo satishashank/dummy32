8a6ec59f2427a491481e116969b4294a87d94a00
shashanksati123 <shashanksati123@gmail.com>
2025-06-29 18:21:24 +0530

    Test: instructions w/o branching

--- test.py
+++ test.py
@@ -12,28 +12,28 @@ program = [
     0x40118233,  # sub x4, x3, x1          ; x4 = x3 - x1 = 255
     0x00120293,  # addi x5, x4, 1          ; x5 = x4 + 1 = 256
 
-    # 0x0021f333,  # and x6, x3, x2          ; x6 = x3 & x2
-    # 0x0021e3b3,  # or x7, x3, x2           ; x7 = x3 | x2
-    # 0x0021c433,  # xor x8, x3, x2          ; x8 = x3 ^ x2
+    0x0021f333,  # and x6, x3, x2          ; x6 = x3 & x2
+    0x0021e3b3,  # or x7, x3, x2           ; x7 = x3 | x2
+    0x0021c433,  # xor x8, x3, x2          ; x8 = x3 ^ x2
 
-    # 0x00f1f493,  # andi x9, x3, 15         ; x9 = x3 & 0xF
-    # 0x0f01e513,  # ori x10, x3, 0xF0       ; x10 = x3 | 0xF0
-    # 0x0ff1c593,  # xori x11, x3, 0xFF      ; x11 = x3 ^ 0xFF
+    0x00f1f493,  # andi x9, x3, 15         ; x9 = x3 & 0xF
+    0x0f01e513,  # ori x10, x3, 0xF0       ; x10 = x3 | 0xF0
+    0x0ff1c593,  # xori x11, x3, 0xFF      ; x11 = x3 ^ 0xFF
 
-    # 0x00129633,  # sll x12, x5, x1         ; x12 = x5 << x1 = 4096
-    # 0x0012d6b3,  # srl x13, x5, x1         ; x13 = x5 >> x1 = 16
-    # 0x4012d733,  # sra x14, x5, x1         ; x14 = x5 >> x1 = 16 (arith)
+    0x00129633,  # sll x12, x5, x1         ; x12 = x5 << x1 = 4096
+    0x0012d6b3,  # srl x13, x5, x1         ; x13 = x5 >> x1 = 16
+    0x4012d733,  # sra x14, x5, x1         ; x14 = x5 >> x1 = 16 (arith)
 
     0x00329793,  # slli x15, x5, 3         ; x15 = x5 << 3 = 2048
     0x0032d813,  # srli x16, x5, 3         ; x16 = x5 >> 3 = 32
     0x4032d893,  # srai x17, x5, 3         ; x17 = x5 >> 3 = 32 (arith)
 
-    # 0x0020a933,  # slt x18, x1, x2         ; x18 = (x1 < x2) = 1
-    # 0x0050a993,  # slti x19, x1, 5         ; x19 = (x1 < 5) = 1
-    # 0x00313a33,  # sltu x20, x2, x3        ; x20 = (x2 < x3) = 1
-    # 0x00a0ba93,  # sltiu x21, x1, 10       ; x21 = (x1 < 10) = 1
+    0x0020a933,  # slt x18, x1, x2         ; x18 = (x1 < x2) = 1
+    0x0050a993,  # slti x19, x1, 5         ; x19 = (x1 < 5) = 1
+    0x00313a33,  # sltu x20, x2, x3        ; x20 = (x2 < x3) = 1
+    0x00a0ba93,  # sltiu x21, x1, 10       ; x21 = (x1 < 10) = 1
 
-    # 0x0032a423,  # sw x3, 8(x5)            ; MEM[x5 + 8] = x3
+    0x0032a423,  # sw x3, 8(x5)            ; MEM[x5 + 8] = x3
 ]
 
 
