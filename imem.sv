d02bf705788a7771002609150879665751ae0e83
shashanksati123 <shashanksati123@gmail.com>
2025-07-09 21:25:14 +0530

    Read code.mem for compiled asm

---
+++ imem.sv
@@ -0,0 +1,12 @@
+module imem (
+    output logic [31:0] rData,
+    input logic [31:0] rAddr
+  );
+  logic [31:0] mem [2047:0];
+  assign rData = mem[rAddr[12:2]];
+  initial
+  begin
+    $readmemh("code.mem",mem);
+  end
+
+endmodule
