ba7b623dec9b7a5a480d7c42f48ac7f5c49b853e
shashanksati123 <shashanksati123@gmail.com>
2025-07-20 01:40:55 +0530

    Logging ints

--- test.c
+++ test.c
@@ -11,26 +11,55 @@ void uart_puts(const char *str)
         uart_putc(*str++);
     }
 }
-int recursive(int x, int y, int var)
+
+void uart_puthex(unsigned int val)
 {
-    // Subracts 14 one by one
-    if (x > (y - 1))
+    uart_puts("0x");
+
+    // Start from most significant nibble (shift = 28 bits)
+    int shift = 28;
+    for (int i = 0; i < 8; i++)
     {
-        return var;
+        unsigned int nibble = (val >> shift) & 0xF;
+        uart_putc(nibble < 10 ? ('0' + nibble) : ('A' + nibble - 10));
+        shift -= 4;
     }
-    else
+    uart_puts("\n");
+}
+void uart_putsi(const char *str, unsigned int val)
+{
+    uart_puts(str);
+    uart_puthex(val);
+}
+
+int looper(int var)
+{
+    int a, b, d;
+    a = 0;
+    d = 0;
+    b = 0;
+    while (d < 4)
     {
-        x = x + 1;
-        var = var - 1;
-        recursive(x, y, var);
+        d++;
+        for (a = 0; a < 5; a++)
+        {
+            b = 0;
+            while (b < 3)
+            {
+                var -= 0x1; // decrement each time
+                b++;
+            }
+        }
     }
+    return var;
 }
 int main()
 {
-    uart_puts("Hello, world From dummy!\n");
-    int a, b, c;
+    int a, b;
+    uart_puts("Hello World ,from dummy!\n");
     a = 0xBEEFCAFE;
-    recursive(0, 14, a);
-    uart_puts("Recursion Complete.\n");
+    uart_putsi("Data initial value:", a);
+    b = looper(a);
+    uart_putsi("Data final value:", b);
     return 0;
 }
