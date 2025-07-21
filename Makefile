3fc31b6ca0efe8a727152b2797f2546275b89a9a
shashanksati123 <shashanksati123@gmail.com>
2025-07-21 21:10:41 +0530

    Passes riscv-tests

--- Makefile
+++ Makefile
@@ -2,31 +2,29 @@ SIM ?= verilator
 TOPLEVEL_LANG ?= verilog
 WAVES = 1
 VERILOG_SOURCES += $(PWD)/rtl/*.sv
-EXTRA_ARGS += --trace --trace-structs --timing -j 8
-.PHONY: all clean sim
-
-# TOPLEVEL is the name of the toplevel module in your Verilog or VHDL file
+EXTRA_ARGS += --trace --trace-structs --trace-fst --timing -j 8
 TOPLEVEL = TB
-
-# MODULE is the basename of the Python test file
 MODULE = test
-all: sim code.mem data.mem code.dump
 
+XLEN ?= 32
+src_dir := ./tests
 
-code.elf: link.ld boot.s test.c
-		riscv32-unknown-elf-gcc -nostdlib -nostartfiles -O0 -march=rv32i -mabi=ilp32 -T link.ld boot.s test.c -o code.elf
-code.dump: code.elf
-		riscv32-unknown-elf-objdump -D code.elf > code.dump
-data.bin: code.elf
-		riscv32-unknown-elf-objcopy -O binary --only-section=.data code.elf data.bin
-code.bin: code.elf
-		riscv32-unknown-elf-objcopy -O binary --only-section=.text code.elf code.bin
+RISCV_PREFIX ?= riscv$(XLEN)-unknown-elf-
+RISCV_GCC ?= $(RISCV_PREFIX)gcc
+TESTS_PATH ?= $(src_dir)/rv32ui
+all: code.mem data.mem sim
+
+code.bin: rv32i_test.elf
+		riscv32-unknown-elf-objcopy -O binary --only-section=.text rv32i_test.elf code.bin
+data.bin: rv32i_test.elf
+		riscv32-unknown-elf-objcopy -O binary --only-section=.data rv32i_test.elf data.bin
 code.mem: code.bin
-		hexdump -v -e '1/4 "%08x\n"' code.bin >code.mem
+		hexdump -v -e '1/4 "%08x\n"' code.bin > code.mem
 data.mem: data.bin
-		hexdump -v -e '1/1 "%04x\n"' data.bin > data.mem
+		hexdump -v -e '1/1 "%02x\n"' data.bin > data.mem
 sim: code.mem data.mem
 include $(shell cocotb-config --makefiles)/Makefile.sim
-clean::
-	rm -rf code* data* results.xml
+
+clean_build:
+	rm -rf code.mem data.mem code.bin data.bin rv32i_test.elf test.dump
 
---
+++ riscv-tests/rv32ui/riscv_test.h
@@ -0,0 +1,79 @@
+#ifndef _ENV_PICORV32_TEST_H
+#define _ENV_PICORV32_TEST_H
+
+#ifndef TEST_FUNC_NAME
+#  define TEST_FUNC_NAME mytest
+#  define TEST_FUNC_TXT "mytest"
+#  define TEST_FUNC_RET mytest_ret
+#endif
+
+#define RVTEST_RV32U
+#define TESTNUM x28
+
+#define RVTEST_CODE_BEGIN		\
+	.text;				\
+	.global TEST_FUNC_NAME;		\
+	.global TEST_FUNC_RET;		\
+	.global uart_putchar;	\
+TEST_FUNC_NAME:				\
+	la sp, 0x10000FFC;		\
+	sw ra, (sp);			\
+	lui	a3,%hi(.test_name);	\
+	addi a3,a3,%lo(.test_name);\
+.prname_next:				\
+	lb	a0,0(a3);			\
+	beq	a0,zero,.prname_done;\
+	jal uart_putchar;		\
+	addi	a3,a3,1;		\
+	jal	zero,.prname_next;	\
+.prname_done:				\
+	addi	a0,zero,'.';	\
+	jal uart_putchar;		\
+	jal uart_putchar;		\
+	lw ra, (sp);			\
+
+#define RVTEST_PASS			\
+	la sp, 0x10000FFC;		\
+	sw ra, (sp);			\
+	addi a0, zero, 'o';		\
+	jal uart_putchar;		\
+	addi a0, zero, 'k';		\
+	jal uart_putchar;		\
+	addi a0, zero, '\r';	\
+	jal uart_putchar;		\
+	addi a0, zero, '\n';	\
+	jal uart_putchar;		\
+	lw ra, (sp);			\
+	jal zero, TEST_FUNC_RET;
+
+
+#define RVTEST_FAIL			\
+	la sp, 0x10000FFC;		\
+	sw ra, (sp);			\
+	addi a0, zero, 'e';		\
+	jal uart_putchar;		\
+	addi a0, zero, 'r';		\
+	jal uart_putchar;		\
+	addi a0, zero, 'r';		\
+    jal uart_putchar;		\
+	addi a0, zero, 'o';		\
+    jal uart_putchar;		\
+	addi a0, zero, 'r';		\
+	jal uart_putchar;		\
+	addi a0, zero, '\r';	\
+	jal uart_putchar;		\
+	addi a0, zero, '\n';	\
+	jal uart_putchar;		\
+	lw ra, (sp);			\
+	jal zero, TEST_FUNC_RET;
+
+#define RVTEST_CODE_END
+#define RVTEST_DATA_BEGIN             \
+	.data;                            \
+	.test_name :.ascii TEST_FUNC_TXT; \
+	.byte 0x00;                       \
+	.balign 4, 0;
+
+#define RVTEST_DATA_END
+
+#endif
