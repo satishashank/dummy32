a2cd19cc4c319bbc4da727eee3955ab373abc5d0
shashanksati123 <shashanksati123@gmail.com>
2025-08-07 22:07:42 +0530

    Gshare fixed

--- rtl/core/branchPredictorGshare.sv
+++ rtl/core/branchPredictorGshare.sv
@@ -1,5 +1,5 @@
 module branchPredictorGshare#(
-    parameter int BTB_ENTRIES = 256,
+    parameter int BTB_ENTRIES = 128,
     parameter int PHT_ENTRIES = 1024,
     parameter int GHR_WIDTH = $clog2(PHT_ENTRIES),
     parameter int BTB_INDEX_WIDTH = $clog2(BTB_ENTRIES),
@@ -26,40 +26,41 @@ module branchPredictorGshare#(
             logic [BTB_TAG_WIDTH-1:0] tag;
             logic [31:0] target;
           } btb_entry;
-  typedef struct packed {
-            logic [1:0] counter;
-          }pht_entry;
 
 
   btb_entry btb_mem [BTB_ENTRIES-1:0];
-  pht_entry pht_mem [PHT_ENTRIES-1:0];
+  logic [1:0] pht_mem [PHT_ENTRIES-1:0];
 
-  logic [GHR_WIDTH-1:0] ghr,pcBitsGhr,phtIndex;
+  logic [GHR_WIDTH-1:0] ghr,pcBitsGhrFetch,pcBitsGhrEx,phtIndexEx,phtIndexFetch;
   logic [BTB_INDEX_WIDTH-1:0] fetchIndex;
   logic [BTB_TAG_WIDTH-1:0]   fetchTag;
   logic [BTB_INDEX_WIDTH-1:0] exIndex;
   logic [BTB_TAG_WIDTH-1:0]   exTag;
   logic exHit;
 
-  logic [1:0] nextCount,count;
+  logic [1:0] nextCount,count,countF;
   localparam [1:0]
-             sNtaken = 2'b00,
-             wNtaken = 2'b01,
-             wTaken = 2'b10,
+             sNtaken = 2'b10,
+             wNtaken = 2'b00, //default not taken
+             wTaken = 2'b01,
              sTaken = 2'b11;
 
 
   assign exIndex = exPc[BTB_INDEX_WIDTH+1:2];
   assign exTag  = exPc[31:BTB_INDEX_WIDTH];
 
-  assign pcBitsGhr = fetchPc[GHR_WIDTH+1:2];
-  assign phtIndex = pcBitsGhr^ghr;
-  assign count = pht_mem[phtIndex].counter;
+  assign pcBitsGhrFetch = fetchPc[GHR_WIDTH+1:2];
+  assign pcBitsGhrEx = exPc[GHR_WIDTH+1:2];
+
+  assign phtIndexFetch = pcBitsGhrFetch^ghr;
+  assign phtIndexEx = pcBitsGhrEx^ghr;
+  assign count = pht_mem[phtIndexEx];
+  assign countF = pht_mem[phtIndexFetch];
 
 
   assign fetchIndex = fetchPc[BTB_INDEX_WIDTH+1:2];
   assign fetchTag  = fetchPc[31:BTB_INDEX_WIDTH];
-  assign fetchHit = (btb_mem[fetchIndex].valid && (btb_mem[fetchIndex].tag == fetchTag))&&pht_mem[phtIndex].counter[1];
+  assign fetchHit = (btb_mem[fetchIndex].valid && (btb_mem[fetchIndex].tag == fetchTag))&&(countF[0]);
   assign fetchTarget = btb_mem[fetchIndex].target;
   assign exHit = btb_mem[exIndex].valid && (btb_mem[exIndex].tag == exTag);
 
@@ -70,22 +71,23 @@ module branchPredictorGshare#(
     begin
       if(exBranch)
       begin
-        ghr<= {ghr[GHR_WIDTH-1:1],exTaken}; //Next ghr
+        ghr <= {ghr[GHR_WIDTH-1:1],exTaken}; //Next ghr
         if (exHit)
         begin
-          pht_mem[phtIndex].counter <= nextCount; //update PHT
+          pht_mem[phtIndexEx] <= nextCount; //update PHT
         end
         else //New branch
         begin
           btb_mem[exIndex].valid <= 1;
           btb_mem[exIndex].tag <= exTag;
           btb_mem[exIndex].target <= exTarget;
-          pht_mem[phtIndex].counter <= exTaken?wTaken:wNtaken; //First pht entry
+          pht_mem[phtIndexEx] <= exTaken?wTaken:wNtaken; //First pht entry
         end
       end
-
     end
   end
+
+
   always_comb
   begin
     case (count)
